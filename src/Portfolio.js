728dbfd284328acf481b3288ad025c2f85cc21f7
Gayathri Karunarathna <karunarathna.gayathri15@gmail.com>
2024-06-03 14:27:32 +0530

    Updated images2

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -12,43 +12,43 @@ const Portfolio = () => {
       title: 'Property Search',
       description: 'A web application to search for properties based on various criteria',
       link: 'https://github.com/Kanch-prog/Estate-Agent/',
-      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_02.png'
+      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_02.PNG'
     },
     {
       title: 'Resume Builder',
       description: 'A web application that allows users to create and customize professional resumes online.',
       link: 'https://kanch-prog.github.io/resume/',
-      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_03.png'
+      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_03.PNG'
     },
     {
       title: 'Data Analysis Project',
       description: 'A proposal to analyze transaction data to identify anomalies that may indicate potential money laundering',
       link: 'https://github.com/Kanch-prog/AML_transacation/',
-      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_04.png'
+      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_04.PNG'
     },
     {
       title: 'Document Management System',
       description: 'A web application developed using Next.js, allowing users to efficiently manage their documents.',
       link: 'https://github.com/Kanch-prog/Doc_manager_NextJS',
-      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_05.png'
+      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_05.PNG'
     },
     {
       title: 'Blogging Platform with RecSys',
       description: 'A dynamic platform developed using Django, with CRUD & Recommendation system included.',
       link: 'https://github.com/Kanch-prog/Blog-App-ML/',
-      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_06.png'
+      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_06.PNG'
     },
     {
       title: 'Small Social Network',
       description: 'A platform developed using Django, allowing users to connect with each other.',
       link: 'https://github.com/Kanch-prog/Social-frontend',
-      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_07.png'
+      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_07.PNG'
     },
     {
       title: 'Django Blogging Platform',
       description: 'A web application built using Django, to create, publish, and manage blog posts, with content management system.',
       link: 'https://github.com/Kanch-prog/Django-crud-app/',
-      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_08.png'
+      imageUrl: 'https://kanch-prog.github.io/portfolio/images/pic_08.PNG'
     },
   ];
 
