0a567c6bb826a5f5906be643339fd15ab36c561e
Kanch-prog <121807277+Kanch-prog@users.noreply.github.com>
2024-06-03 14:12:46 +0530

    Update Portfolio.js

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -8,7 +8,7 @@ const Portfolio = () => {
       title: 'Travel-Landing-page',
       description: 'Developed a dynamic landing page for a tour and travel website, featuring an auto-playing background video slider.',
       link: 'https://kanch-prog.github.io/background-video/',
-      imageUrl:`${baseUrl}images/pic_01.png` 
+      imageUrl:`images/pic_01.png` 
     },
     {
       title: 'Property Search',
